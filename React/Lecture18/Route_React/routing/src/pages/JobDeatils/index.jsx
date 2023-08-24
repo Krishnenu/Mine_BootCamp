989393360d27fb59db5290feb5d0021b77570f3f
Krishnenu <krishnendu.nr@gmail.com>
2023-08-24 22:53:15 +0530

    name updated

--- React/Lecture18/Route_React/routing/src/pages/JobDeatils/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const JobDetails=()=>{
-    return (
-        <h1>hiiii</h1>
-    )
-}
-
-export default JobDetails;
--- React/Lecture18/Route_React/routing/src/pages/NoMatch/index.jsx
+++
@@ -1,6 +0,0 @@
- export default ()=>{
-    return (
-        <h1>No Page Found .. .. FallBack Page</h1>
-    )
-
-}
