c8fc5e31daa5c822605e953bb4432ce8d2d9dcb9
Krishnenu <krishnendu.nr@gmail.com>
2023-09-16 20:45:51 +0530

    Promis Concept in detail added

--- BootCamp_100_Preparation/JS Concepts/callBack.js
+++ BootCamp_100_Preparation/JS Concepts/callBack.js
@@ -1,29 +1,28 @@
-function callMeBack(refCb){
-    setTimeout(()=>{
-        console.log("Hellow World");
-    },1000);
+function callMeBack(refCb) {
+  setTimeout(() => {
+    console.log("Hellow World 1");
+  }, 1000);
 
-    console.log("Dummey");
+  console.log("Logged");
 
-    setTimeout(()=>{
-        console.log("Hellow World 3");
-    },2000);
+  setTimeout(() => {
+    console.log(" Hellow World 3 ");
+  }, 2000);
+  
+  refCb();
 
-    refCb();
+  setTimeout(() => {
+    console.log(" Hellow World 2 ");
+  }, 1000);
 
-    setTimeout(()=>{
-        console.log("Hellow World 2");
-    },1000);
-
-    refCb();
+  refCb();
 }
 
-function useMeRef(){
-    setTimeout(()=>{
-        console.log("Hellow World 4");
-    },1000);
+
+function useMeRef() {
+  setTimeout(() => {
+    console.log(" CallBack Reference function");
+  }, 1000);
 }
 
 callMeBack(useMeRef);
-
-
